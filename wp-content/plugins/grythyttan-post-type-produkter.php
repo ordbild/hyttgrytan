777cc8384c383ad41babb6efb711bd057b7e29b8
Stefan Ledin <stefan.ledin@gmail.com>
2016-03-10 19:49:08 +0100

    Loopar ut info om produktserierna

---
+++ wp-content/plugins/grythyttan-post-type-produkter.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Plugin name: Post type Produkter
+ */
+add_action( 'init', 'create_post_type_produkter' );
+function create_post_type_produkter()
+{
+    register_post_type( 'produkter', array(
+        'labels' => array(
+            'name' => 'Produkter',
+            'singular_name' => 'Produkt'
+        ),
+        'public' => true,
+        'supports' => array( 'title' ),
+        'menu_icon' => 'dashicons-cart'
+    ) );
+
+    register_taxonomy( 'produktserier', 'produkter', array(
+        'label' => 'Produktserier',
+        'public' => false,
+        'show_ui' => true 
+    ) );
+}
+
