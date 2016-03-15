419cbbf5807be6c41f7d0989553dc2d87ad83730
Stefan Ledin <stefan.ledin@gmail.com>
2016-03-15 13:31:32 +0100

    Mer optimeringar + effekter!

--- wp-content/themes/grythyttan/includes/enqueue-scripts-styles.php
+++ wp-content/themes/grythyttan/includes/enqueue-scripts-styles.php
@@ -68,6 +68,7 @@ function get_scripts()
         $scripts[] = $assetsDir . '/js/slick.min.js';
         $scripts[] = $assetsDir . '/js/jquery.lazyloadxt.js';
         $scripts[] = $assetsDir . '/js/jquery.lazyloadxt.bg.js';
+        $scripts[] = $assetsDir . '/js/jquery.lazyloadxt.srcset.js';
         $scripts[] = $assetsDir . '/js/app.js';
     } else {
         $scripts[] = $assetsDir . '/js/min/scripts-min.js';
